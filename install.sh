9ed0d86700b48d3b87a9d24ebfa5fec747e87b4a
Russell Murray <russ@codelogic.ws>
2016-06-13 23:51:59 -0700

    Added Tmux

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ echo . ~/.pairConfig/scripts/bashPairConfig >> ~/.bashrc
 echo source ~/.pairConfig/vimConfig/vimrc.local >> ~/.vimrc.local
 echo source ~/.pairConfig/vimConfig/vimrc.before.local >> ~/.vimrc.before.local
 echo source ~/.pairConfig/vimConfig/vimrc.bundles.local >> ~/.vimrc.bundles.local
+echo source "~/.pairConfig/tmuxConfig/tmux.conf" >> ~/.tmux.conf
